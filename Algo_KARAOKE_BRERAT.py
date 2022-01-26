56af5d65cb06dd8ebae76a5766bf3aba72eec771
Martin Brerat <m.brerat@rns.ecolescreatives.com>
2022-01-26 14:20:03 +0100

    Create Algo_KARAOKE_BRERAT.py

---
+++ Algo_KARAOKE_BRERAT.py
@@ -0,0 +1,39 @@
+class Player:
+    def __init__(self,):
+        pass
+
+    def numeroChanson(self, numeroChanson):
+        print("Chanson numero "+ numeroChanson)
+
+    def scoreTotal(self, scoreTotal):
+        print("Score total")    
+
+    def Player(self, player):
+        print("joueur numero")
+
+    def scorePlayer(self, scoreJoueur):
+        print("Score Joueur")
+
+
+chanson0 = numeroChanson("1")
+chanson1 = numeroChanson("2")
+chanson2 = numeroChanson("3")
+chanson3 = numeroChanson("4")
+chanson4 = numeroChanson("5")
+
+joueur.Player1
+joueur.Player2
+joueur.Player3
+
+scoreJoueur1 = 50
+scoreJoueur2 = 50
+scoreJoueur3 = 50
+
+scoreTotale = scoreJoueur1 + scoreJoueur2 + scoreJoueur3
+
+print("le score totale est de " scoreTotale )
+
+
+class Karaoke:
+    def __init__(self) -> None:
+        pass
